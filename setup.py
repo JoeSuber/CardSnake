01abfeadd998c778a983fdb2372d8da228a949e0
Joseph Suber <joesuber@gmail.com>
2016-01-29 15:23:31 -0600

    attempting to figure out setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name='CardSnake',
+        version='.1',
+        packages=['requests', 'grequests', 'Levenshtein', 'gmpy2', 'numpy', 'cv2'],
+        url='http://www.31stcenturygames.com',
+        license='MIT',
+        author='Joe Suber',
+        author_email='joesuber@gmail.com',
+        description='scrape together data & images of Magic: the Gathering cards. ID them by '
+)
