c709a4259d0f8d2dcfe130afc162fb54e7dec1b0
joseph suber <joesuber@gmail.com>
2016-02-13 20:43:35 -0600

    setup note

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 """
 gmp2 may need the gnu-mpc c library installed before it will work
 grequests needs greenlet, which needs a c compiler to install itself
-cv2 is opencv - 2.4.x or 3.x - cv2.so or cv2.dll should be placed in the 'site-packages' your python interp. is using
+cv2 is opencv - 2.4.x or 3.x - cv2.so or cv2.pyd should be placed in the 'site-packages' your python interp. is using
 numpy and python-Levenshein are also in need of a compiler for install
 """
 
