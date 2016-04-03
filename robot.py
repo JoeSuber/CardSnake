eae9af25c349d027cfcf195ba6c5d1fdf5782985
joesuber <joesuber@gmail.com>
2016-04-03 07:17:47 -0500

    better sensors, better sense

--- robot.py
+++ robot.py
@@ -118,8 +118,8 @@ class Posts(object):
             saying = "Use Arrow Keys and Numbers (1-4) to adjust until warp is square"
             for pnt, r, c, ln in zip(self.pts1, self.radii, self.colors, self.lines):
                 cv2.circle(cam_img, tuple(pnt), r, c, thickness=ln)
-            for line, (label, pnt, hugh) in enumerate(zip(["p1", "p2", "p3", "p4"], self.pts1, self.colors)):
-                draw_str(cam_img, (10, 40 + (line * 15)), " =".join([label, str(tuple(pnt))]), hugh)
+            for line, (label, pnt, hue) in enumerate(zip(["p1", "p2", "p3", "p4"], self.pts1, self.colors)):
+                draw_str(cam_img, (10, 40 + (line * 15)), " =".join([label, str(tuple(pnt))]), color=hue)
         else:
             saying = "Press [a] to turn corner-adjustment on/off."
         draw_str(cam_img, (20, 17), saying)
@@ -143,44 +143,45 @@ class Posts(object):
 
 
 class Robot(object):
+    """
+    for initializing and running the interface between cpu and robot firmware via serial link
+    """
     def __init__(self, baud='115200', port='/dev/ttyACM0', readtimer=0, nl='\n', LOAD=True):
         self.baud = baud
         self.port = port
         self.con = ser.Serial(port=port, baudrate=baud, timeout=readtimer)
         self.nl = nl
         self.LOADING = LOAD
-        self.do = {'pickup_pos': 'M280 S120 P0' + nl + ' G0 X1',
-                   'drop_pos': 'G0 X45',
+        self.do = {'pickup_pos': 'G0 X1',
+                   'drop_pos': 'G0 X52',
                    'fan_on': 'M106',
                    'fan_off': 'M107',
                    'servo_drop': 'M280 S57 P0',
                    'servo_up': 'M280 S120 P0',
                    'end_stop_status': 'M119',
                    'positions': 'M114',
-                   'stop': 'M410',
-                   'raise_hopper': 'G92 Y3' + nl + ' G1 Y0'}
-        self.times = {'pickup_pos': 2.7,
-                      'drop_pos': 2.6,
+                   'stop': 'M410'}
+        self.times = {'pickup_pos': 2.9,
+                      'drop_pos': 2.9,
                       'fan_on': 0.1,
                       'fan_off': 0.1,
                       'servo_drop': 0.6,
                       'servo_up': 0.5,
-                      'end_stop_status': 0.02,
-                      'positions': 0.011,
-                      'stop': 0.02,
-                      'raise_hopper': 0.0}
+                      'end_stop_status': 0.06,
+                      'positions': 0.06,
+                      'stop': 0.02}
         self.BLOCKED = False
         time.sleep(0.4)
         self.con.write("M115" + self.nl)    # M115 info string request
         time.sleep(0.5)
         print("serial port: {}   isOpen={}".format(self.con.getPort(), self.con.isOpen()))
-        for l in self.con.read(size=self.con.inWaiting()).split(':'):
-            print(": {}".format(l))
         # physically home X (arm) Y (hopper) and Z (output bin) to zero positions
-        self.con.write("G28 XYZ" + self.nl)
+        self.con.write("G28 XZ" + nl)
+        self.con.write("G28 Y" + nl)
         time.sleep(0.5)
-        self.con.write(self.do['drop_pos'] + self.nl + " " + self.do['servo_up'] + self.nl)  # arm out to allow loading
-        self.con.write(self.do['fan_off'] + self.nl)
+        # arm out to allow loading
+        self.con.write(self.do['drop_pos'] + nl + " " + self.do['servo_up'] + nl)
+        self.con.write(self.do['fan_off'] + nl)
         self.NEED_DROP = False
         self.SHOULD_RETURN = False
         self.ID_DONE = False
@@ -188,9 +189,14 @@ class Robot(object):
         self.bins = OrderedDict([('Low', 125), ('High', 247.5)])
         self.bin_cuts = OrderedDict([('Low', 0.0), ('High', 0.5)])
         self.bin_sliver = 0.2
-        self.con.flush()
+        self.LOADING = True
+        tl = self.con.readline()
+        while tl:
+            print("startup: {}".format(tl.strip()))
+            tl = self.con.readline()
 
     def dothis(self, instruction):
+        """sends instruction to robot and returns the estimated execution time if available"""
         if instruction in self.do.keys():
             self.con.write(self.do[instruction] + self.nl)
             return self.times[instruction]
@@ -198,50 +204,131 @@ class Robot(object):
         self.con.write(instruction + self.nl)
         return 0.0
 
-    def bin_lookup(self, price, binname='Low'):
+    def bin_lookup(self, price, binname=None):
+        """returns the bin-name the card-price should be sorted into"""
         for bk, bv in self.bin_cuts.viewitems():
             if price >= bv:
                 binname = bk
         return binname
 
-    def sensor_tripped(self, term='x_max: TRIGGERED', ret_size=109):
+    def sensor_stats(self, min_ret=99):
+        """returns dict of end-stop sensors, keyed by sensor name, with values of 'open' or 'TRIGGERED'"""
         wait = self.dothis('end_stop_status') + time.time()
-        while time.time() < wait:
-            if self.con.inWaiting() > ret_size:
-                return term in self.con.read(size=self.con.inWaiting())
-        print("time expired on call to sensor_tripped()")
-        return False
+        # start = time.time()
+        # inw = self.con.inWaiting()
+        while (time.time() < wait) and (self.con.inWaiting() < min_ret):
+            pass
+            # if inw:
+                # print "inwaiting: ", inw
+            # inw =
+        # print("actual speed: {}, ret: {}".format(time.time() - start, self.con.inWaiting()))
+        return dict([tuple(chunk.split(": ")) for chunk in self.con.read(size=self.con.inWaiting()).split(self.nl)
+                    if (': ' in chunk) and (('_min' in chunk) or ('_max' in chunk))])
 
-    def xyz_pos(self, ret_size=55):
-        self.con.flushInput()
+    def xyz_pos(self, min_ret=59):
+        """ returns dict of current stepper DESTINATIONS (in float(mm)) keyed by single-letter axis names"""
         wait = self.dothis("positions") + time.time()
-        while time.time() < wait:
-            if self.con.inWaiting() > ret_size:
-                dd = {}
-                for c in self.con.read(size=self.con.inWaiting()).split(' Count')[0].split(' '):
-                    d = c.replace('ok', '').replace('\n', '').replace('n', '')
-                    if ':' in d:
-                        a, b = d.split(':')
-                        dd[a.strip()] = b.strip()
-                print dd
-                return dd
-        print("time expired on call to xyz_pos()")
-        return {}
+        # start = time.time()
+        must_have = ['X', 'Y', 'Z', 'E']
+        xyz_dict = {}
+        while time.time() < wait and (self.con.inWaiting() < min_ret):
+            pass
+        # finalwait = self.con.inWaiting()
+        for positions in [ps.split(' Count ')[0] for ps in self.con.read(size=self.con.inWaiting()).split(self.nl)
+                          if ' Count ' in ps]:
+            if all([axis in positions for axis in must_have]):
+                for p in positions.split(" "):
+                    if ":" in p:
+                        k, v = p.split(":")
+                        xyz_dict[k.strip()] = float(v.strip())
+        # print("actual speed: {}, ret: {}".format(time.time() - start, finalwait))
+        return xyz_dict or self.xyz_pos(min_ret=min_ret-1)
 
-    def go_z(self, bin_name, timeconst=0.041):
+    def go_z(self, bin_name, timeconst=0.043):
+        """given a destination bin, position everything for the drop, while decrementing for the next drop into the bin and
+        return the estimated time from the present when the drop can happen"""
         newz = float(self.bins[bin_name])
         self.bins[bin_name] -= self.bin_sliver
-        curz = float(self.xyz_pos()['Z'])
+        curz = self.xyz_pos()['Z']
         x_spot = self.do['drop_pos'].split(' ')[1]
-        x_time = self.times['drop_pos']
         z_time = abs(curz - newz) * timeconst
-        self.dothis("G1 Z" + str(newz) + " " + x_spot + self.nl)
-        return z_time if z_time > x_time else x_time
+        self.dothis("G1 Z" + str(newz) + " " + x_spot)
+        return z_time if z_time > self.times['drop_pos'] else self.times['drop_pos']
 
-    def load_hopper(self, move=5.0, top="y_max: TRIGGERED", bottom="y_min: TRIGGERED"):
+    def hopper_up(self, y_current=None, bite=1.5, timeconst=0.5):
+        """ raise the input hopper by a little bit, return the seconds it is estimated to take"""
+        if y_current is None:
+            try:
+                y_current = self.xyz_pos()['Y']
+            except KeyError:
+                print("WARNING: hopper_up couldn't get 'Y' starting position. Moving to zero + 1.")
+                y_current = 0
+        self.dothis("G0 Y{}".format(y_current + bite))
+        return bite * timeconst
+
+    def load_hopper(self, move=10.0, y_top=220):
         """ load cards until bottom switch is triggered, indicating max capacity, but only move
         down while top proximity sensor is triggered. Set self.LOADING false when done"""
-        return self.raise_hopper()
+        # first move up until proximity sensor is triggered to get the platform up top
+        must_have = ['y_min', 'y_max']
+        print("Movin' on up (until top sensor triggered)")
+        self.dothis("G0 Y{}".format(y_top))
+        INITIALIZE_UP = True
+        while INITIALIZE_UP:
+            sensor = self.sensor_stats()
+            if all([mh in sensor.keys() for mh in must_have]):
+                if 'TRIGGERED' in sensor['y_max']:
+                    print("top sensor = {}".format(sensor['y_max']))
+                    time.sleep(self.dothis("stop"))
+                    INITIALIZE_UP = False
+        xyz = self.xyz_pos()
+        print("Okay, now load the Hopper. Loading ends when bottom limit switch is triggered.")
+        print("Positions:  {}".format(", ".join([k + ":" + str(v) for k, v in xyz.viewitems()])))
+        new_sweep = True
+        destination = max((xyz['Y'] - move), 0)
+        start = time.time()
+        while self.LOADING:
+            sensor = self.sensor_stats()
+            if all([mh in sensor.keys() for mh in must_have]):
+                if 'TRIGGERED' in sensor['y_min']:
+                    self.dothis("stop")
+                    self.dothis("G92 Y0")
+                    self.dothis("G0 Y0")
+                    self.LOADING = False
+                    continue
+                if 'TRIGGERED' in sensor['y_max'] and new_sweep:
+                    print("moving down to: Y={}".format(destination))
+                    self.dothis("G0 Y{}".format(destination))
+                    start = time.time()
+                    new_sweep = False
+                if 'open' in sensor['y_max'] and not new_sweep:
+                    print("top sensor Open after {} seconds...".format(time.time()-start))
+                    new_sweep = True
+                    xyz = self.xyz_pos()
+                    if 'Y' in xyz.keys():
+                        destination = max((xyz['Y'] - move), 0)
+                    else:
+                        print("BAD XYZ: {}".format(", ".join([k + ":" + str(v) for k, v in xyz.viewitems()])))
+        xyz = self.xyz_pos()
+        print("DONE LOADING")
+        print("Positions:  {}".format(", ".join([k + ":" + str(v) for k, v in xyz.viewitems()])))
+        nudge_up = True
+        wait = 0
+        sensor = None
+        while not sensor:
+            time.sleep(.1)
+            sensor = self.sensor_stats()
+        while nudge_up:
+            if time.time() > wait:
+                wait = self.hopper_up() + time.time()
+                sensor = None
+                while not sensor:
+                    time.sleep(.01)
+                    sensor = self.sensor_stats()
+            if "TRIGGERED" in sensor['y_max']:
+                nudge_up = False
+        time.sleep(self.dothis('fan_on'))
+        return self.hopper_up(bite=0.2)
 
 
 def main():
@@ -260,6 +347,8 @@ def main():
     wait = time.time()
     bin = "Nada"
     OLD_YMIN = False
+    time.sleep(0.5)
+    robot.load_hopper()
     while True:
         __, frame = cam.read()
         showimg = eyeball.draw_guides(frame.copy())
@@ -269,17 +358,21 @@ def main():
         ch = cv2.waitKey(1) & 0xff
         eyeball.check_key(ch)
         if ch == 27:
+            robot.dothis("stop")
             robot.dothis("fan_off")
             cv2.destroyAllWindows()
             break
+        if ch == ord('q'):
+            print robot.sensor_stats(min_ret=100)
+        if ch == ord('w'):
+            print robot.xyz_pos()
         if ch == ord('g') and not robot.ID_DONE:
             print ("[g] pressed")
             matcher, cardlist = card_adder(smile.handful(warp), matcher, orientation.orient_db, cardlist,
                                        maxitems=MAX_ITEMS)
             current_kp, matchdict = card_compare(warp, looker, matcher)
-            bestmatch = sorted([(indx, matches)
-                                for indx, matches in matchdict.viewitems() if len(matches) > MIN_MATCHES],
-                               key=lambda x: len(x), reverse=True)
+            bestmatch = sorted([(i, matches) for i, matches in matchdict.viewitems() if len(matches) > MIN_MATCHES],
+                               key=lambda x: len(x[1]), reverse=True)
             for indx, matches in bestmatch:
                 one_card = cardlist[indx]
                 pricestr = 'None'
@@ -291,7 +384,7 @@ def main():
                 robot.ID_DONE = True
                 bin = robot.bin_lookup(pricetag)
                 cv2.imshow("{} {}".format(one_card.name, one_card.code), cv2.drawMatchesKnn(warp, current_kp,
-                                                  cv2.imread(os.path.join(pathfront, one_card.pic_path)),
+                                                cv2.imread(os.path.join(pathfront, one_card.pic_path)),
                                                   one_card.kp, matches,
                                                   outImg=np.zeros((eyeball.yc, eyeball.xc * 2, 3), dtype=np.uint8),
                                                   flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS))
@@ -307,19 +400,16 @@ def main():
         if robot.ID_DONE and (not robot.PICKING_UP) and (time.time() > wait):
             wait = robot.dothis("pickup_pos") + time.time()
             robot.PICKING_UP = True
-        if robot.PICKING_UP and (not robot.NEED_DROP) and (time.time() > wait) and robot.sensor_tripped():
-            wait = robot.go_z(bin) + time.time()
+        if robot.PICKING_UP and (not robot.NEED_DROP) and (time.time() > wait):
+            wait = robot.dothis("servo_up") + time.time()
+            wait += robot.go_z(bin)
             robot.PICKING_UP = False
             robot.ID_DONE = False
             robot.NEED_DROP = True
         if robot.NEED_DROP and (time.time() > wait):
             wait = robot.dothis("servo_drop") + time.time()
+            robot.hopper_up(bite=0.4)
             robot.NEED_DROP = False
-        YMIN = robot.sensor_tripped(term='y_min: TRIGGERED')
-        if OLD_YMIN != YMIN:
-            OLD_YMIN = YMIN
-            if YMIN:
-                robot.dothis("raise_hopper")
 
 
 if __name__ == "__main__":
